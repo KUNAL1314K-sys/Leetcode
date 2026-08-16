5e2c28d81fb6999e6805f370b843cbec893c4822
Kunal Vardhan <kunalvardhan1435@gmail.com>
2026-08-16 21:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (38.98%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.c
+++ 724-find-pivot-index/find-pivot-index.c
@@ -1,19 +1,20 @@
 int pivotIndex(int* nums, int numsSize) {
-    for (int pivot = 0; pivot < numsSize; pivot++) {
-        int leftsum = 0;
-        int rightsum = 0;
+    int totalSum = 0;
 
-        for (int i = 0; i < pivot; i++) {
-            leftsum += nums[i];
+    for (int i = 0; i < numsSize; i++) {
+        totalSum += nums[i];
+    }
+
+    int leftSum = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int rightSum = totalSum - leftSum - nums[i];
+
+        if (leftSum == rightSum) {
+            return i;
         }
 
-        for (int i = pivot + 1; i < numsSize; i++) {
-            rightsum += nums[i];
-        }
-
-        if (leftsum == rightsum) {
-            return pivot;
-        }
+        leftSum += nums[i];
     }
 
     return -1;
